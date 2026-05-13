104f0946868db01acd100fceae2d0fd199793c3e
jinfenghua1990 <jinfenghua1990@gmail.com>
2026-05-14 00:38:15 +0800

    fix: switch cockpit entry to stable phase-2.7d app

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
+import App2_7D from './src/App2_7D';
 import './src/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App2_7D />
   </React.StrictMode>
 );
